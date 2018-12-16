01b8c80a1a84454bb7fb8ffb5109d7bec03f51c7
kuroganechong <kuroganechong@gmail.com>
2018-12-16 21:22:21 +0800

    navbar

--- src/js/calc.js
+++ src/js/calc.js
@@ -470,3 +470,13 @@ $.getJSON("data/hero.json").done(function (data) {
         }
     })
 });
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
--- src/js/custom.js
+++ src/js/custom.js
@@ -767,3 +767,14 @@ function setVue() {
         }
     })
 }
+
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
